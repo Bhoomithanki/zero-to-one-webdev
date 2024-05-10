47a255fddfcd4a48d25c42a8326df1a19f99c72b
Bhoomi Thanki <139106129+Bhoomithanki@users.noreply.github.com>
2024-05-10 11:48:55 +0530

    Add files via upload
    Fundamentals of JavaScript

---
+++ Fundamentals of Javascript/Primitive data types in JavaScript.js
@@ -0,0 +1,34 @@
+// Numbers :The number data type represents numeric values. It can include integers, floating-point numbers,and special values like NaN (Not-a-Number) and Infinity.
+
+let value1 = 2 //integers
+let value2 = 7.5 //Floating-point
+let value3 = NaN //Not-a-number
+let value4 = Infinity //infinity 
+let sum = value1 + value2
+console.log(sum)
+console.log(value3)
+console.log(value4)
+
+
+//string :The string data type represents sequences of characters enclosed in single (' '), double (" "), or backticks (` `)quotes. Strings are used to represent text data.
+
+let string1 = "Bhoomi"
+let string2 = "Thanki"
+let fullstring = string1 + string2
+console.log(fullstring)
+
+// boolean :The boolean data type has only two values, true and false, and is used to represent logical values. Booleans are commonly used for conditions and comparisons.
+
+let value = 2
+let value0 = "2"
+console.log(value == value0)
+
+//null :The null data type represents the intentional absence of any object value or a value that has been explicitly set to indicate no value. It is often used to initialize variables before assigning meaningful values.
+
+let num = null
+console.log(num)
+
+//undefined :The undefined data type represents a variable that has been declared but has not been assigned a value. It is often the initial value of variables.
+
+let name
+console.log(name)
---
+++ Fundamentals of Javascript/Variables in Javascript.js
@@ -0,0 +1,8 @@
+// Purpose of declaring varaiable in Javascript 
+//  Main purpose :The purpose of declaring variables in JavaScript is to reserve a memory location for storing data values that can be used throughout a program. Variables act as placeholders for data, making it easier to work with and manipulate information in your code.
+// declaring with 'let' keyword
+let name; // Declaration
+name = "bhoomi"; // Initialization
+let name1 = "bhoomika" //declaration and initilization
+// we declare the varaiable name means we reserve memory location for varaiable name means which ever value will be assigned to name will be located at the reserve location and can be used through out a program
+
